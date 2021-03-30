f5eba071249e620b9e5a0c94db9d693a4fed2cb6
JJ <inbox4development@gmail.com>
2021-03-30 11:03:48 -0400

    Upload old projects

--- projects_school/project_ConnectingCars/Vehicle.java
+++ projects_school/project_ConnectingCars/Vehicle.java
@@ -1,17 +1,3 @@
-/**
-	Name:	Dexter Falcon
-	Student ID:	009 055 534
-	CPS 209 Assignment 2
-   
-   	NOTE:
-   	SELECTING ITEM		= Make sure to click/select the item by clicking at the front end of the item.
-   	DRAGGING TO LINK 	= Make sure to drop close to the middle of the body of the last rail car in the trailer.
-   	CONTAINER - ADD		= This removes the container from the pile and add to the rail car.
-   	CONTAINER - REMOVE	= This removes the container from the rail car and return to the pile.
-	Q IMPLEMENTATION	= First in, First out.  When a container is returned to the pile, it is will be added at the end.
-	
-    
-*/
 
 import java.awt.geom.Ellipse2D ;
 import javax.swing.JComponent;
--- projects_school/project_PasswordHashing/README.md
+++ projects_school/project_PasswordHashing/README.md
@@ -1,2 +1,8 @@
 # Password-Hash
 CPS633 Computer Security Assignment => Password Hashing and Authentication
+
+Illustrates the use of hashing to authenticate a user and to encode/decode a password with the following options:
+
+1. Authenticate user via OLM Hash
+2. Authenticate user via Client Response Authorization
+3. Crack user password
